97951cf869510acfe63c47756a3e8997884488a4
Lucas Tai-MacArthur <lucastai@seas.upenn.edu>
2015-04-01 23:32:59 -0400

    Finished Math

--- app/src/main/java/com/example/zkrasner/skibuddy/WeightedTimeAverage.java
+++ app/src/main/java/com/example/zkrasner/skibuddy/WeightedTimeAverage.java
@@ -1,13 +1,43 @@
 package com.example.zkrasner.skibuddy;
 
+import java.util.ArrayList;
+
 /**
  * Created by Lucas on 4/1/15.
  */
 public class WeightedTimeAverage {
 
+
+    private ArrayList<Integer> measurements;
+    private ArrayList<Long> times;
+
     public WeightedTimeAverage(){
+        measurements = new ArrayList<Integer>();
+        times = new ArrayList<Long>();
 
+    }
 
+    public void shoveThatDirtyMeasurementInMyArrayList(int input){
+        measurements.add(input);
+        times.add(System.currentTimeMillis());
+
+    }
+
+    public double returnMovingAverageToMyWaitingBrain(){
+        int count = 0;
+        long total = 0;
+        // need error handling here
+        for(int i = 0; i < measurements.size(); i++){
+            int curr_meas = measurements.get(i);
+            long timestamp = times.get(i);
+            if(System.currentTimeMillis() - timestamp  < 1.8E6){
+                count++;
+                total += curr_meas;
+            }
+
+        }
+
+        return (double) count / total;
     }
 
 }
